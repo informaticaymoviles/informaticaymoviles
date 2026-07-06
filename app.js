267f404ea2ef733ddcf2a749f3fb3df5383d3cb0
informaticaymoviles <informaticaymoviles@gmail.com>
2026-07-06 18:40:34 +0200

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+/*
+ * informaticaymoviles.com - Buscador de presupuestos de hardware y móviles.
+ * Copyright (C) 2026  [Tu Nombre o Nombre del Proyecto]
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 // Base de datos local simulada (En producción, esto se puede alimentar de una API o scraping)
 const database = {
     pc: [
